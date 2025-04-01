2680907fe3aa46d21d2376e901396b7c3b38fbbf
Jeremy <jeremy.libert@luxapps.lu>
2025-04-01 18:16:09 +0200

    Fix voter

--- src/Security/AbstractVoter.php
+++ src/Security/AbstractVoter.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace LibertJeremy\Symfony\SecurityHelpersBundle\Security;
 
 use LibertJeremy\Symfony\SecurityHelpers\Traits\VoterHelperTrait;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
-abstract class AbstractVoter
+abstract class AbstractVoter extends Voter
 {
     use VoterHelperTrait;
 }
